6599236589e76e5581a9738629225b08a760c48b
Ross Harper <ross.harper@bbc.co.uk>
2017-02-17 17:40:55 +0000

    Push URLSession singleton usage up to factory

--- raspberrysauce-ios/Networking/NetworkingFactory.swift
+++ raspberrysauce-ios/Networking/NetworkingFactory.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class NetworkingFactory {
     static func createNetworking() -> Networking {
-        return UrlSessionNetworking()
+        return UrlSessionNetworking(URLSession.shared)
     }
     
     struct AuthManagerTokenProvider : TokenProvider {
--- raspberrysauce-ios/Networking/UrlSessionNetworking.swift
+++ raspberrysauce-ios/Networking/UrlSessionNetworking.swift
@@ -10,36 +10,42 @@ import Foundation
 
 class UrlSessionNetworking : Networking {
 
+    let urlSession : URLSession
+
+    init(_ urlSession: URLSession) {
+        self.urlSession = urlSession
+    }
+
     func get(url: URL, onSuccess: @escaping (_ responseBody: Data) -> Void, onError: @escaping (Int?) -> Void) {
         self.get(url: url, headers: nil, onSuccess: onSuccess, onError: onError)
     }
-    
+
     func get(url: URL, headers: [String : String]?, onSuccess: @escaping (_ responseBody: Data) -> Void, onError: @escaping (Int?) -> Void) {
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
-        
+
         self.doRequest(request: &request, headers: headers, onSuccess: onSuccess, onError: onError)
     }
-    
+
     func post(url: URL, body: String, onSuccess: @escaping (_ responseBody: Data) -> Void, onError: @escaping (Int?) -> Void) {
         self.post(url: url, headers: nil, body: body, onSuccess: onSuccess, onError: onError)
     }
-    
+
     func post(url: URL, headers: [String : String]?, body: String, onSuccess: @escaping (_ responseBody: Data) -> Void, onError: @escaping (Int?) -> Void) {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.httpBody = body.data(using: .utf8)
-        
+
         self.doRequest(request: &request, headers: headers, onSuccess: onSuccess, onError: onError)
     }
-    
+
     private func doRequest(request: inout URLRequest, headers: [String : String]?, onSuccess: @escaping (_ responseBody: Data) -> Void, onError: @escaping (Int?) -> Void) {
         if headers != nil {
             for key in headers!.keys {
                 request.addValue(headers![key]!, forHTTPHeaderField: key)
             }
         }
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        let task = urlSession.dataTask(with: request) { data, response, error in
             guard let data = data, error == nil else {                                                 // check for fundamental networking error
                 onError(nil)
                 return
